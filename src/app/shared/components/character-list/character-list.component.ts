5192487862fdd38ec66112598105a58ccb1d895d
Angel Leon <angel@leanware.io>
2023-06-14 22:57:12 -0500

    added character list

---
+++ src/app/shared/components/character-list/character-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
+
+@Component({
+  selector: 'app-character-list',
+  templateUrl: './character-list.component.html',
+  styleUrls: ['./character-list.component.scss'],
+})
+export class CharacterListComponent implements OnInit {
+  items: string[] = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.generateItems();
+  }
+
+  private generateItems() {
+    const count = this.items.length + 1;
+    for (let i = 0; i < 50; i++) {
+      this.items.push(`Item ${count + i}`);
+    }
+  }
+
+  onIonInfinite(ev: any) {
+    this.generateItems();
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { HomePageRoutingModule } from '../home/home-routing.module';
+import { CharacterListComponent } from './components/character-list/character-list.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, CharacterListComponent],
   imports: [CommonModule, IonicModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, CharacterListComponent],
 })
 export class SharedModule {}
